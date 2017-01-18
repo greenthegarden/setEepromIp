a733349ea6ad080634a779491011f1c828e28f81
Philip Cutler <greenthegarden@gmail.com>
2017-01-18 21:11:58 +1030

    added a method to test network settings

--- setEepromIp/setEepromIp.ino
+++ setEepromIp/setEepromIp.ino
@@ -8,25 +8,32 @@
 byte mac[6] = { 0xCA, 0xFE, 0xBA, 0xBE, 0x00, 0x01 };
 
 
-void setMac()
-{
+void setMac() {
+  Serial.print("Setting mac address ...");
   NetEeprom.writeDhcpConfig(mac);
 }
 
-void setManualIpConfiguration()
-{
+void setManualIpConfiguration() {
   // additional parameters
   byte ip[4] = { 192, 168, 1, 100 };
   byte dns[4] = { 8, 8, 8, 8 };
   byte gw[4] = { 192, 168, 1, 1 };
   byte subnet[4] = { 255, 255, 255, 0 };
 
+  Serial.println("Configuring network settings ...");
   NetEeprom.writeManualConfig(mac, ip, dns, gw, subnet);
+}
+
+void testNetwork() {
+  NetEeprom.begin();
+  Serial.print("IP Address: ");
+  Serial.println(Ethernet.localIP());
 }
 
 void setup() {
   // put your setup code here, to run once:
   setMac();
+  testNetwork();
 }
 
 void loop() {
